24127367fbf42d58f48141ab8cf587fc483af097
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-03-21 23:20:40 +0530

    usenavigation

--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const  Contact = () => {
+
+    const navigate = useNavigate(); //programmatic navigation
+
     return (
         <div>
             <h1>Contact</h1>
+
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => navigate("/about")}
+            >go to about
+            </button>
         </div>
     );
 };
