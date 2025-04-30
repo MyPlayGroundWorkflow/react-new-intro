b116be468fa398f4b69a7b0f184ed8496528cb22
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-04-30 13:17:10 +0530

    add order slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,19 @@
 import {configureStore} from "@reduxjs/toolkit";
 import customerSlice from "./slice/CustomerSlice.ts";
+import authSlice from "./slice/authSlice.ts";
+import itemSlice from "./slice/ItemSlice.ts";
+import cartSlice from "./slice/cartSlice.ts";
+import orderSlice from "./slice/orderSlice.ts";
 
 
 const store =
     configureStore({
     reducer: {
+        auth: authSlice,
         customer : customerSlice,
-
+        items: itemSlice,
+        cart: cartSlice,
+        orders: orderSlice
     }
 })
 
