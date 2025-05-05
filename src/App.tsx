1c0107087d5f89d07a482b2f89f2aa19424a16b0
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-05-06 00:08:55 +0530

    dashboard done

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import DashboardLayout from "./components/layouts/DashboardLayout.tsx";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Customers from "./pages/Customers.tsx";
 import Items from "./pages/Items.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 function App() {
 
     return (
             <Router>
                 <Routes>
                     <Route element={<DashboardLayout />}>
-                        <Route path="/" element={<DashboardLayout />} />
+                        <Route path="/" element={<Dashboard />} />
                         <Route path="/customers" element={<Customers />} />
                         <Route path="/items" element={<Items />} />
                     </Route>
